7d5b5d7d5a5ff5d6baf636a5dad9fd6b9ddf6237
ridoyislamnasim <ridoyislamnasim@gmail.com>
2023-11-14 16:04:20 +0600

    app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose
     .catch((err) => console.log(err));
 
 // routing setup
-app.use("/admin", admin);
+app.use("/", admin);
 
 // Start the server
 app.listen(process.env.PORT || 2000, () => {
