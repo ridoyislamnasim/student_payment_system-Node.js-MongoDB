5d35a9aa79de3144dc6b60278c52e2fb09afbcaa
ridoyislamnasim <ridoyislamnasim@gmail.com>
2023-11-14 15:42:31 +0600

    router created

---
+++ router/adminRouter.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const admin = express.Router();
+const mongoose = require("mongoose");
+// controller
+const { createStudent, deleteStudent, allStudent, editStudent, searchStudent, login } = require("../controller/adminController");
+const authMiddleware = require("../midelware/authMiddleware");
+
+
+// ============================= logIn admin ======================
+admin.post('/login',
+    login
+);
+// ============================= create student ======================
+admin.post('/create-student',
+    authMiddleware,
+    createStudent
+);
+// ============================= delete  student ======================
+admin.delete('/delete-student/:id',
+    authMiddleware,
+    deleteStudent
+);
+// ============================= all student information get ======================
+admin.get('/all-student',
+    authMiddleware,
+    allStudent
+);
+// ============================= update student information  ======================
+admin.put('/update-student/:id',
+    authMiddleware,
+    editStudent
+);
+// ============================= search student information use student name   ======================
+admin.get('/search-studnet/:name',
+    authMiddleware,
+    searchStudent
+);
+
+module.exports = admin;
